981e84c2093e3ef542a167188320507600c224e9
Lenche Petrov <lenchegjeorgieva7@gmail.com>
2022-01-09 00:59:58 +0200

    some small fix

---
+++ WorkForceManagement/WorkForceManagement.BLL/Exceptions/ItemDoesNotExistException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace WorkForceManagement.BLL
+{
+    [Serializable]
+    internal class ItemDoesNotExistException : Exception
+    {
+        public ItemDoesNotExistException()
+        {
+        }
+
+        public ItemDoesNotExistException(string message) : base(message)
+        {
+        }
+
+        public ItemDoesNotExistException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ItemDoesNotExistException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
