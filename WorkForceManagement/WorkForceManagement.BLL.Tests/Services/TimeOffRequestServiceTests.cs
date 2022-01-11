e2ab1a76f37677cdeb5d585ba452b1ed6a795f37
Lenche Petrov <lenchegjeorgieva7@gmail.com>
2022-01-11 15:42:52 +0200

    tests-base

---
+++ WorkForceManagement/WorkForceManagement.BLL.Tests/Services/TimeOffRequestServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkForceManagement.BLL.Services;
+using WorkForceManagement.DAL.Entities;
+using WorkForceManagement.DAL.Repositories;
+using Xunit;
+
+namespace WorkForceManagement.BLL.Tests.Services
+{
+ public class TimeOffRequestServiceTests
+    {
+        [Fact]
+        public async Task CreateAsync_ValidRequest_Passes()
+        {
+            //arrange
+            var requestRepositoryStub = new Mock<IRepository<TimeOffRequest>>();
+            
+
+            var sut = new TimeOffRequestService(requestRepositoryStub.Object);
+
+            TimeOffRequest timeOffRequest = new TimeOffRequest();
+            //act
+            var result = await Record.ExceptionAsync(() => sut.CreateTimeOffRequest(timeOffRequest));
+
+            Assert.Null(result);
+            //asert
+        }
+    }
+}
