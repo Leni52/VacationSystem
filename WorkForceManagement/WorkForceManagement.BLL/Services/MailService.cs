09071b5b8f5297a53560289a2bc5be03499b1921
Lenche Petrov <lenchegjeorgieva7@gmail.com>
2022-01-15 01:17:29 +0200

    email implementation-start

---
+++ WorkForceManagement/WorkForceManagement.BLL/Services/MailService.cs
@@ -0,0 +1,43 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using WorkForceManagement.DAL;
+using WorkForceManagement.DAL.Entities;
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
+
+namespace WorkForceManagement.BLL.Services
+{
+    public class MailService : IMailService
+    {
+        private readonly MailSettings _mailSettings;
+        public MailService(IOptions<MailSettings> options)
+        {
+            _mailSettings = options.Value;
+        }
+        public async Task SendEmail(MailRequest mailRequest)
+        {
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
+            email.Subject = mailRequest.Subject;
+            var builder = new BodyBuilder();
+            builder.HtmlBody = mailRequest.Body;
+            email.Body = builder.ToMessageBody();
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port,
+                SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+            await smtp.SendAsync(email);
+            smtp.Disconnect(true);
+
+        }
+
+    }
+}
---
+++ WorkForceManagement/WorkForceManagement.WEB/Controller/EmailController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using WorkForceManagement.BLL.Services;
+using WorkForceManagement.DAL;
+
+namespace WorkForceManagement.WEB.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IMailService _mailService;
+        public EmailController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+        [HttpPost]
+        public async Task<ActionResult> Send([FromForm] MailRequest request)
+        {
+            try
+            {
+                await _mailService.SendEmail(request);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+    }
+}
