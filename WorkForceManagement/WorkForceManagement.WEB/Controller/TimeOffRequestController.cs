46176913df6f8e8272983b7d4b556ccd27fc563d
Altin Axhami <altinnaxhami@gmail.com>
2022-01-21 17:53:41 +0100

    When TOR is created its status is Created and not Awaiting

--- WorkForceManagement/WorkForceManagement.WEB/Controller/TimeOffRequestController.cs
+++ WorkForceManagement/WorkForceManagement.WEB/Controller/TimeOffRequestController.cs
@@ -17,14 +17,15 @@ namespace WorkForceManagement.WEB.Controller
     public class TimeOffRequestController : ControllerBase
     {
         private readonly ITimeOffRequestService _timeOffRequestService;
-        private readonly IUserService _userService;      
+        private readonly IUserService _userService;
         private readonly IMapper _mapper;
+
         public TimeOffRequestController(
-            ITimeOffRequestService timeOffRequestService,           
+            ITimeOffRequestService timeOffRequestService,
             IUserService userService,
             IMapper mapper)
         {
-            _timeOffRequestService = timeOffRequestService;         
+            _timeOffRequestService = timeOffRequestService;
             _userService = userService;
             _mapper = mapper;
         }
@@ -44,7 +45,7 @@ namespace WorkForceManagement.WEB.Controller
         public async Task<ActionResult<TimeOffRequestResponseDTO>> GetTimeOffRequest(Guid timeOffRequestId)
         {
             TimeOffRequest requestFromDB = await _timeOffRequestService.GetTimeOffRequest(timeOffRequestId);
-            
+
             var requestModel = _mapper.Map<TimeOffRequestResponseDTO>(requestFromDB);
 
             return Ok(requestModel);
@@ -70,20 +71,20 @@ namespace WorkForceManagement.WEB.Controller
         public async Task<ActionResult> UpdateTimeOffRequest(Guid timeOffRequestId, TimeOffRequestRequestDTO request)
         {
             User currentUser = await _userService.GetCurrentUser(User);
-          //  TimeOffRequest timeOffRequest = await _timeOffRequestService.GetTimeOffRequest(timeOffRequestId);
+            //  TimeOffRequest timeOffRequest = await _timeOffRequestService.GetTimeOffRequest(timeOffRequestId);
             if (!ModelState.IsValid)
             {
                 return BadRequest();
             }
             // _mapper.Map(request, timeOffRequest);
             TimeOffRequest timeOffRequest = _mapper.Map<TimeOffRequest>(request);
-             var updatedRequest = await _timeOffRequestService.UpdateTimeOffRequest(timeOffRequestId, timeOffRequest, currentUser.Id);
+            var updatedRequest = await _timeOffRequestService.UpdateTimeOffRequest(timeOffRequestId, timeOffRequest, currentUser.Id);
             return Ok();
         }
 
         [HttpDelete("{timeOffRequestId}")]
         [Authorize(Policy = "TimeOffRequestCreator")]
-        public async  Task<ActionResult> DeleteTimeOffRequest(Guid timeOffRequestId)
+        public async Task<ActionResult> DeleteTimeOffRequest(Guid timeOffRequestId)
         {
             if (!(Guid.Empty == timeOffRequestId))
             {
@@ -139,7 +140,7 @@ namespace WorkForceManagement.WEB.Controller
             if (currentUser != null)
             {
                 var requestsDTO = _mapper.Map<List<TimeOffRequestResponseDTO>>(currentUser.TimeOffRequestsToApprove);
-                
+
                 return Ok(requestsDTO);
             }
 
